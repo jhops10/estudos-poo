c27eace6ec3b3b09b3b28c2f4585e5336b65d2ff
João Paulo <joaowais@gmail.com>
2024-04-07 20:39:49 -0300

    aula 11a - exemplo de classe abstrata

---
+++ src/Herança/Exercício - 11a/Pessoa/Pessoa.ts
@@ -0,0 +1,30 @@
+export abstract class Pessoa {
+  private nome!: string;
+  private idade!: number;
+  private sexo!: string;
+
+  public getNome() {
+    return this.nome;
+  }
+  public setNome(nome: string) {
+    this.nome = nome;
+  }
+
+  public getIdade() {
+    return this.idade;
+  }
+  public setIdade(idade: number) {
+    this.idade = idade;
+  }
+
+  public getSexo() {
+    return this.sexo;
+  }
+  public setSexo(sexo: string) {
+    this.sexo = sexo;
+  }
+
+  public fazerAniv() {
+    this.setIdade(this.getIdade() + 1);
+  }
+}
